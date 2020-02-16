594ec0b6aebb98bd2be1a18dd977b34aa7fec236
Syakiran407 <mahmudsyakiran407@gmail.com>
2020-02-16 17:02:42 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+var rsvpListener = null;
+var guestbookListener = null;
+
+// Add Firebase project configuration object here
+// var firebaseConfig = {};
+  // web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCidWslseI7a9dCNFSShrVPZDLMm3xR_d8",
+    authDomain: "fir-web-codelab-5e1dd.firebaseapp.com",
+    databaseURL: "https://fir-web-codelab-5e1dd.firebaseio.com",
+    projectId: "fir-web-codelab-5e1dd",
+    storageBucket: "fir-web-codelab-5e1dd.appspot.com",
+    messagingSenderId: "755886290702",
+    appId: "1:755886290702:web:a7d6e7c6ddd86c1259095f"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// firebase.initializeApp(firebaseConfig);
+
+
+// FirebaseUI config
+const uiConfig = {
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  signInOptions: [
+    // Email / Password Provider.
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl){
+      // Handle sign-in.
+      // Return false to avoid redirect.
+      return false;
+    } 
+  }
+};
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+//Listen to RSVP button clicks
+startRsvpButton.addEventListener("click", () => {
+  if (firebase.auth().currentUser) {
+    //User is signed in; allows user to sign out
+    firebase.auth().signOut();
+  } else {
+    //No user is signed in; allow user to sign in 
+    ui.start("#firebaseui-auth-container", uiConfig);
+  }
+});
+
+//Listen to the current Auth storage
+firebase.auth().onAuthStateChanged((user)=> {
+  if (user){
+    startRsvpButton.textContent = "LOGOUT"
+  }
+  else {
+    startRsvpButton.textContent = "RSVP"
+  }
+});
